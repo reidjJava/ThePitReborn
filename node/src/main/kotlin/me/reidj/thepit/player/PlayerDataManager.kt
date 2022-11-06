739754677f4995a9fda8d17d09515f4a95ae91dc
reidjJava <reidjJava@gmail.com>
2022-11-07 00:44:13 +0300

    Новый красивый скорборд

--- node/src/main/kotlin/me/reidj/thepit/player/PlayerDataManager.kt
+++ node/src/main/kotlin/me/reidj/thepit/player/PlayerDataManager.kt
@@ -1,7 +1,10 @@
 package me.reidj.thepit.player
 
 import me.func.mod.Anime
+import me.func.mod.ui.token.Token
+import me.func.mod.ui.token.TokenGroup
 import me.func.mod.util.after
+import me.func.protocol.data.emoji.Emoji
 import me.func.protocol.ui.indicator.Indicators
 import me.reidj.thepit.app
 import me.reidj.thepit.attribute.AttributeUtil
@@ -12,6 +15,7 @@ import me.reidj.thepit.player.prepare.PrepareMods
 import me.reidj.thepit.player.prepare.PreparePlayerBrain
 import me.reidj.thepit.protocol.BulkSaveUserPackage
 import me.reidj.thepit.protocol.SaveUserPackage
+import me.reidj.thepit.util.Formatter
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -28,6 +32,18 @@ class PlayerDataManager : Listener {
     val userMap = mutableMapOf<UUID, User>()
 
     var prepares: MutableSet<Prepare> by notNull()
+
+
+    private val group = TokenGroup(
+        Token.builder()
+            .title("Монеты")
+            .content { player -> Emoji.COIN + "§6 " + Formatter.toFormat(app.getUser(player)!!.stat.money) }
+            .build(),
+        Token.builder()
+            .title("Рейтинг")
+            .content { player -> Emoji.DONATE_WHITE }
+            .build()
+    )
 
     init {
         prepares = mutableSetOf(PrepareMods(), PreparePlayerBrain)
@@ -54,6 +70,9 @@ class PlayerDataManager : Listener {
 
         after(5) {
             Anime.hideIndicator(player, Indicators.ARMOR, Indicators.EXP, Indicators.HEALTH, Indicators.HUNGER)
+
+            group.subscribe(player)
+
             prepares.forEach { it.execute(user) }
             player.inventory.addItem(AttributeUtil.generateAttribute(ItemManager.items["TEST2"]!!))
             player.inventory.addItem(AttributeUtil.generateAttribute(ItemManager.items["TEST3"]!!))
---
+++ node/src/main/kotlin/me/reidj/thepit/util/Formatter.kt
@@ -0,0 +1,14 @@
+package me.reidj.thepit.util
+
+import java.text.DecimalFormat
+
+/**
+ * @project : ThePitReborn
+ * @author : Рейдж
+ **/
+object Formatter {
+
+    private val doubleFormat = DecimalFormat("#,###.##")
+
+    fun toFormat(double: Double): String = doubleFormat.format(double)
+}
