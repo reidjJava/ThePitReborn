c94559a02448257789393856044137e22754f967
reidjJava <reidjJava@gmail.com>
2022-12-05 18:21:43 +0300

    FIX BLYAT

--- node/src/main/kotlin/me/reidj/thepit/player/prepare/PreparePlayerBrain.kt
+++ node/src/main/kotlin/me/reidj/thepit/player/prepare/PreparePlayerBrain.kt
@@ -2,12 +2,14 @@ package me.reidj.thepit.player.prepare
 
 import me.func.mod.ui.menu.button
 import me.func.mod.ui.menu.dailyReward
+import me.func.mod.util.after
 import me.reidj.thepit.app
 import me.reidj.thepit.attribute.AttributeType
 import me.reidj.thepit.attribute.AttributeUtil
 import me.reidj.thepit.content.DailyReward
 import me.reidj.thepit.player.User
 import me.reidj.thepit.util.teleportAndPlayMusic
+import org.bukkit.attribute.Attribute
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.potion.PotionEffect
@@ -21,32 +23,35 @@ import ru.cristalix.core.formatting.Formatting
 object PreparePlayerBrain : Prepare {
 
     override fun execute(user: User) {
-        spawnTeleport(user.player)
-
+        val player = user.player
         val stat = user.stat
         val now = System.currentTimeMillis()
 
-        // Обнулить комбо сбора наград если прошло больше суток или комбо > 7
-        if (stat.rewardStreak > 0 && now - stat.lastEnter * 10000 > 24 * 60 * 60 * 1000 || stat.rewardStreak > 6) {
-            stat.rewardStreak = 0
+        spawnTeleport(player)
+
+        after(10) {
+            // Обнулить комбо сбора наград если прошло больше суток или комбо > 7
+            if (stat.rewardStreak > 0 && now - stat.lastEnter * 10000 > 24 * 60 * 60 * 1000 || stat.rewardStreak > 6) {
+                stat.rewardStreak = 0
+            }
+            if (now - stat.dailyClaimTimestamp * 10000 > 14 * 60 * 60 * 1000) {
+                stat.dailyClaimTimestamp = now / 10000
+                dailyReward {
+                    currentDay = stat.rewardStreak
+                    storage = DailyReward.values().map {
+                        button {
+                            title = it.title
+                            item = it.itemStack
+                        }
+                    }.toMutableList()
+                }.open(player)
+                val dailyReward = DailyReward.values()[stat.rewardStreak]
+                player.sendMessage(Formatting.fine("Ваша ежедневная награда: " + dailyReward.title))
+                dailyReward.give(user)
+                stat.rewardStreak++
+            }
+            stat.lastEnter = now / 10000
         }
-        if (now - stat.dailyClaimTimestamp * 10000 > 14 * 60 * 60 * 1000) {
-            stat.dailyClaimTimestamp = now / 10000
-            dailyReward {
-                currentDay = stat.rewardStreak
-                storage = DailyReward.values().map {
-                    button {
-                        title = it.title
-                        item = it.itemStack
-                    }
-                }.toMutableList()
-            }.open(user.player)
-            val dailyReward = DailyReward.values()[stat.rewardStreak]
-            user.player.sendMessage(Formatting.fine("Ваша ежедневная награда: " + dailyReward.title))
-            dailyReward.give(user)
-            stat.rewardStreak++
-        }
-        stat.lastEnter = now / 10000
     }
 
     fun getSpawnLocation() = app.worldMeta.label("spawn")!!
@@ -57,12 +62,15 @@ object PreparePlayerBrain : Prepare {
         it.z += 0.5
     })
 
+    private fun getMaxHealth(player: Player) = player.getAttribute(Attribute.GENERIC_MAX_HEALTH)
+
+
     private fun setMaxHealth(player: Player, maxHealth: Double) {
-        player.maxHealth = 20.0 + maxHealth
+        getMaxHealth(player).baseValue = 20.0 + maxHealth
     }
 
     fun setHealth(player: Player, health: Double) {
-        player.health = player.maxHealth.coerceAtMost(player.health + health)
+        player.health = getMaxHealth(player).baseValue.coerceAtMost(player.health + health)
     }
 
     fun setMoveSpeed(player: Player, speed: Double) {
